768b9a355119cec45255640cb268b51809e0cc9b
Suvendu-code <134857463+Suvendu-code@users.noreply.github.com>
2023-09-16 07:51:46 +0530

    Add files via upload

---
+++ E-shopping/Data/imageSlider.js
@@ -0,0 +1,23 @@
+export const imageSlider = [
+    {
+       img : "https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/75afae751a444eaf.jpg?q=50",
+       link : "https://www.flipkart.com/air-conditioners/pr?sid=j9e%2Cabm%2Cc54&p%5B%5D=facets.capacity%255B%255D%3D1.5.Ton&p%5B%5D=facets.energy_rating%255B%255D%3D5Star&otracker=hp_bannerads_1_2.bannerAdCard.BANNERADS_AC-HPW2-APPnDT-CAP1_8DA3R7D9DXKC"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/d6dc40011f48d2da.jpg?q=50",
+        link : "https://www.flipkart.com/zebronics-pixaplay-18-projector-store?otracker=hp_bannerads_2_2.bannerAdCard.BANNERADS_App_MC5EHBBLD8H2"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/84ae27f93c14a4e3.jpg?q=50",
+        link : "https://www.flipkart.com/travel/flights?param=FKDTHPWNOV223AIRLINESNKDJBDJKKJUYBBHDFSv3&otracker=hp_bannerads_3_2.bannerAdCard.BANNERADS_f_SE5FJZ9QCWTP"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/bf743b1798a4c119.jpg?q=50",
+         link : "https://www.flipkart.com/mobiles/~cs-yzf76mtmzm/pr?sid=tyy%2C4io&collection-tab-name=vivo+T2+5G&param=19837&otracker=hp_bannerads_1_2.bannerAdCard.BANNERADS_a_41TIJAX54A3P"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/a1d93b6bc446790d.jpg?q=50",
+         link : "https://www.flipkart.com/travel/flights?param=FKHPtravelHPWFLYNEW18OFFDOMFLI&otracker=hp_bannerads_5_2.bannerAdCard.BANNERADS_a_D6RSUUQZBZQ1"
+    }
+
+]
